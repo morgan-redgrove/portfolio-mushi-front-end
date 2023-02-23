acbf737a464b65f848d5a9244e6926e61363c4c3
MorganRedgrove <morganekelunddavies@gmail.com>
2023-02-23 09:35:12 +0000

    api calls WIP

--- components/screens/MapScreen.jsx
+++ components/screens/MapScreen.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { getReports } from "../../utils/ApiCalls";
 
 function MapScreen() {
+  const [ reports, setReports ] = useState([])
+
+  useEffect(() => {
+    getReports().then((reports) => {
+      setReports(reports)
+      console.log(reports)
+    })
+  }, [])
+
   return <Text>MapScreen</Text>;
 }
 
