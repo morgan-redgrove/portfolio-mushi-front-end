a85216ce77e56549d65e17a990657da2a813853d
MorganRedgrove <morganekelunddavies@gmail.com>
2023-02-23 12:23:10 +0000

    render report working

--- components/screens/ReportScreen.jsx
+++ components/screens/ReportScreen.jsx
@@ -1,11 +1,11 @@
 import React, {useState, useEffect} from "react";
-import { StyleSheet, Text, View, Button, Image } from "react-native";
+import { StyleSheet, Text, View, Button, Image, FlatList } from "react-native";
 import { getReportById } from "../../utils/ApiCalls";
 
 function ReportScreen({route}) {
     const { id } = route.params
 
-    const [report, setReport] = useState({})
+    const [report, setReport] = useState(null)
 
     useEffect(() => {
         getReportById(id)
@@ -15,37 +15,48 @@ function ReportScreen({route}) {
     }, [])
 
     // console.log(report)
-    const { username, location, time_stamp, notes, species,alternate_species, credibility, prevalence } = report
-    console.log(username)
-    console.log(location)
-    console.log(time_stamp)
-    console.log(notes)
-    console.log(species)
-    console.log(alternate_species)
-    console.log(credibility)
-    console.log(prevalence)
+
+    // const { username, location, time_stamp, notes, species,alternate_species, credibility, prevalence } = report
+    // console.log(username)
+    // console.log(location)
+    // console.log(time_stamp)
+    // console.log(notes)
+    // console.log(species)
+    // console.log(alternate_species)
+    // console.log(credibility)
+    // console.log(prevalence)
 
 
+    if (!report) {
+        return(
+            <View>
+                <Text>Loading...</Text>
+            </View>
+        )
+    } else {
+       return (
+            <View>
+                <Text>lat: {report.location.lat}  long: {report.location?.long}</Text>
+                <Text>username: {report.username}</Text>
+                <Text>timestamp: {report.time_stamp}</Text>
+                <Text>notes: {report.notes}</Text>
+                <Text>species: {report.species.species} votes: {report.species.votes}</Text>
+                <Text>credibility: {report.credibility}</Text>
+                <Text>alternate species:</Text>
+                <FlatList
+                    data={report.alternate_species}
+                    renderItem={({item: {species, votes}} ) => (
+                        <View>
+                            <Text>   - species: {species}  votes: {votes}</Text>
+                        </View>
+ 
+                    )}
+                />
 
-  return (
-    <View>
-      <Text>lat: {report?.location?.lat}  long: {report?.location?.long}</Text>
-      <Text>username: {report?.username}</Text>
-      <Text>timestamp: {report?.time_stamp}</Text>
-      <Text>notes: {report?.notes}</Text>
-      <Text>species: {report?.species.species} votes: {report?.species.votes}</Text>
-      <Text>credibility: {report?.credibility}</Text>
-
-      <FlatList
-        data={report?.alternate_species}
-        renderItem={({ species }) => (
-            <Text>species: {species.species} votes: {species.votes}</Text>
-        )}
-      />
-
-      <Text>prevalence{report?.prevalence}</Text>
-    </View>
-  );
+                <Text>prevalence: {report.prevalence}</Text>
+            </View>
+        )  
+    }
 }
 
 export default ReportScreen;
