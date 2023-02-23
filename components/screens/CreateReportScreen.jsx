7fbf5505870bb5b7d04268d458502b76fdc03f8a
fishertom <tfisher92@gmail.com>
2023-02-23 14:56:27 +0000

    api util, basic page layout, some functionality for create report page

--- components/screens/CreateReportScreen.jsx
+++ components/screens/CreateReportScreen.jsx
@@ -1,8 +1,52 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import { SelectList } from "react-native-dropdown-select-list";
+import { getMushrooms, postReport } from "../../utils/ApiCalls";
 
 function CreateReportScreen() {
-  return <Text>CreateReportScreen</Text>;
+  const [species, setSpecies] = useState([]);
+  const [selected, setSelected] = useState("");
+  const [note, setNote] = useState("");
+
+  useEffect(() => {
+    getMushrooms().then((mushrooms) => {
+      setSpecies(
+        mushrooms.map((mushroom) => {
+          return { value: mushroom.commonName };
+        })
+      );
+    });
+  }, []);
+
+  function submitReport() {
+    //postReport()
+  }
+
+  return (
+    <View>
+      <Text>Create a new report</Text>
+      <SelectList
+        setSelected={(val) => setSelected(val)}
+        data={species}
+        save="value"
+      />
+      <TextInput
+        style={styles.textInput}
+        multiline={true}
+        numberOfLines={4}
+        onChangeText={(text) => setNote({ text })}
+        value={note}
+      />
+      <Button title="Add Report" onPress={submitReport} />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  textInput: {
+    backgroundColor: "black",
+    color: "white",
+  },
+});
 
 export default CreateReportScreen;
--- utils/ApiCalls.jsx
+++ utils/ApiCalls.jsx
@@ -12,7 +12,33 @@ export const getReports = () => {
 };
 
 export const getReportById = (id) => {
-    return apiCaller.get(`/reports/${id}`).then((response) => {
-      return response.data.report[0];
-    });
+  return apiCaller.get(`/reports/${id}`).then((response) => {
+    return response.data.report[0];
+  });
+};
+
+export const getMushrooms = () => {
+  return apiCaller.get("/mushrooms").then((response) => {
+    return response.data.mushrooms;
+  });
+};
+
+export const postReport = (
+  location,
+  img_url,
+  username,
+  time_stamp,
+  species
+) => {
+  const report = {
+    location: location,
+    img_url: img_url,
+    username: username,
+    time_stamp: time_stamp,
+    species: species,
   };
+  console.log(report, "<<< Poseted report");
+  return apiCaller.post("/report", report).then((response) => {
+    return response.data.report;
+  });
+};
