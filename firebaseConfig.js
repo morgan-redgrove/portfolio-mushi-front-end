99d2851cf85c1289c773426efa68cb02dbc15e51
fishertom <tfisher92@gmail.com>
2023-03-17 13:18:32 +0000

    config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,7 +4,6 @@ import { getAuth } from "firebase/auth";
 import { getStorage, ref } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyByCxQouyWQuA8We0KWlXQT7BV97gwrU7w",
